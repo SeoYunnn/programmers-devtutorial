35e6de93938eadfed5f5eb35b1c8516ebfe35738
seoyun-lee <20212025@o365.hanbat.ac.kr>
2026-01-16 01:11:38 +0900

    feat: Node.js와 MariaDB 연동

--- web-tutorial/index.js
+++ web-tutorial/index.js
@@ -1,5 +1,8 @@
 import server from "./server.js";
 import router from "./router.js";
 import requestHandler from "./requestHandler.js";
+import mariadb from "./database/connect/mariadb.js";
+
+mariadb.connect();
 
 server.start(router.route, requestHandler);
--- web-tutorial/requestHandler.js
+++ web-tutorial/requestHandler.js
@@ -1,5 +1,15 @@
+import mariadb from "./database/connect/mariadb.js";
+
 function main(res) {
   console.log("main");
+
+  mariadb.query("SELECT * FROM product", function (err, rows) {
+    if (err) {
+      console.log("에러 발생 상세 내용:", err);
+      return;
+    }
+    console.log(rows);
+  });
 
   res.writeHead(200, {"Content-Type": "text/html"});
   res.write("Main Page");
